d2694bc538b90f5c922ac1083eece34c56902abc
ijs01140 <24949850+ijs01140@users.noreply.github.com>
2020-05-09 11:45:53 +0900

    1.15.2 Update

--- update/download_latest_papermc.sh
+++ update/download_latest_papermc.sh
@@ -4,6 +4,6 @@ script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 server_dir="$HOME/server"
 
 cd $script_dir
-wget https://papermc.io/ci/job/Paper-1.14/lastSuccessfulBuild/artifact/paperclip.jar \
+wget https://papermc.io/ci/job/Paper-1.15/lastSuccessfulBuild/artifact/paperclip.jar \
 && rm "$server_dir/paperclip.jar" \
 && mv "$script_dir/paperclip.jar" $server_dir
