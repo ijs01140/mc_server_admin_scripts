36fcf08d50326f954c404388126c4055763276ae
ijs01140 <24949850+ijs01140@users.noreply.github.com>
2020-06-15 13:37:05 +0900

    avoid error when paperclip.jar does not exist

--- update/download_latest_papermc.sh
+++ update/download_latest_papermc.sh
@@ -5,5 +5,5 @@ server_dir="$HOME/server"
 
 cd $script_dir
 wget https://papermc.io/ci/job/Paper-1.15/lastSuccessfulBuild/artifact/paperclip.jar \
-&& rm "$server_dir/paperclip.jar" \
+&& rm -f "$server_dir/paperclip.jar" \
 && mv "$script_dir/paperclip.jar" $server_dir
